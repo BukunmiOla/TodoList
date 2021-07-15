e9f68883d4d68340a19862a7c7b6a9934a63847e
BuknumiOla <ebukunmiola@gmail.com>
2021-07-15 16:15:43 +0100

    Initial Commit

---
+++ TodoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Bukunmi Ola on 15/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
