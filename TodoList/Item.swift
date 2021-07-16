191f4f0c832c8f4867c2c20e1424e85bdd0fa534
BuknumiOla <ebukunmiola@gmail.com>
2021-07-16 23:06:33 +0100

    Table controller for list created

---
+++ TodoList/Item.swift
@@ -0,0 +1,8 @@
+//
+//  Item.swift
+//  TodoList
+//
+//  Created by Bukunmi Ola on 15/07/2021.
+//
+
+import Foundation
---
+++ TodoList/ItemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ItemTableViewCell.swift
+//  TodoList
+//
+//  Created by Bukunmi Ola on 15/07/2021.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
